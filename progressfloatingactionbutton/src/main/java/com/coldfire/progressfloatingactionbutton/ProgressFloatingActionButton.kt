0f1d7acecba353fc14bbea817bc2fef1dbdd28a4
Oliver Rhyme Guibone Añasco <35908542+Bberx@users.noreply.github.com>
2019-12-31 23:38:17 +0800

    Prepare for FAB progress bar library

--- progressfloatingactionbutton/src/main/java/com/coldfire/progressfloatingactionbutton/ProgressFloatingActionButton.kt
+++ progressfloatingactionbutton/src/main/java/com/coldfire/progressfloatingactionbutton/ProgressFloatingActionButton.kt
@@ -1,4 +1,39 @@
+/*
+ * Copyright 2019 Oliver Rhyme G. Añasco
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.coldfire.progressfloatingactionbutton
 
-class ProgressFloatingActionButton {
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ProgressFloatingActionButton(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+    FloatingActionButton(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(
+        context,
+        attrs,
+        R.attr.floatingActionButtonStyle
+    )
+
+
+
+
 }
